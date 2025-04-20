668a6f4b54085e0aba54777967f84abfa5e680ea
ueno <ueno.hug@gmai.com>
2025-04-20 21:18:41 +0900

    Route /（トップページ）の静的にビルドができないため、動的レンダリングに切り替え
    Next.jsの静的生成（SSG）との非互換
    Next.jsはデフォルトでページを静的に生成しようとします（SSG）
    しかし「no-store」や「revalidate: 0」を使ったfetchがあると、「このページはリクエストごとにサーバーで生成しなければならない」と判断し静的生成を拒否するため、ビルドにエラーとなります
    トップページに export const dynamic = "force-dynamic"; を指定して動的レンダリングにして、APIのデータを最新に保ちます

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+/*
+ * dynamicをforce-dynamicに設定します
+ * getFunds関数で「no-store」を使ったfetchがあるため「このページはリクエストごとにサーバーで生成しなければならない」と判断し静的生成を拒否するため
+*/
+export const dynamic = "force-dynamic";
+
 import { Suspense } from "react"
 import FundList from "@/components/fund-list"
 import { getFunds } from "@/lib/api"
