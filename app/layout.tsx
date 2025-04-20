9d2ed253c7345c39b34fcda54d6366f9c6505115
ueno <ueno.hug@gmai.com>
2025-04-20 17:20:18 +0900

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,23 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "Fund Tracker",
+  description: "Fund Tracker",
+  generator: "Fund Tracker",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
